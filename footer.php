01dd457b2c884a0c1d078bce1b28c4d0b5f073eb
VARSHINI A <anv.varshini@gmail.com>
2024-06-14 11:26:26 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p>&copy; 2023 CHC IT Consultancy. All rights reserved.</p>
+            </div>
+            <div class="col-md-6">
+                <ul class="list-inline text-right">
+                    <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                    <li class="list-inline-item"><a href="#">Terms of Service</a></li>
+                    <li class="list-inline-item"><a href="contact.html">Contact Us</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
